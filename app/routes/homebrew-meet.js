e703e5e782703951452cde9879b292eed4ee650a
Jared <szklenskij@allegheny.edu>
2019-05-02 03:33:46 -0400

    revert

--- app/routes/homebrew-meet.js
+++ app/routes/homebrew-meet.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  init() {
-    this._super(...arguments);
-
-    var meet = this.get('hombrew-meet');
-    this.set('meetId', `compontnet-id${hombrew-meet}`);
-  }
 });
